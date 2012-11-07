3ca9db2b4f0db52b8c35168ffc26b3cec16c0370
Istvan Reguly <regulyistvan@gmail.com>
2012-11-07 20:29:28 +0000

    merged two loops in spaceDiscretization, got rid of two temps

--- sp/EvolveValuesRK2_2.h
+++ sp/EvolveValuesRK2_2.h
@@ -1,20 +1,30 @@
 inline void EvolveValuesRK2_2(const float *dT, float *outConservative, //OP_RW, discard
             float *inConservative, //OP_READ, discard
-            float *midPointConservative, //OP_READ, discard
+            float *midPointConservative, //OP_RW, null
             float *out) //OP_WRITE
 
 {
-  outConservative[0] = 0.5*(outConservative[0] * *dT + midPointConservative[0] + inConservative[0]);
-  outConservative[1] = 0.5*(outConservative[1] * *dT + midPointConservative[1] + inConservative[1]);
-  outConservative[2] = 0.5*(outConservative[2] * *dT + midPointConservative[2] + inConservative[2]);
+  float temp[4];
+  temp[0] = 0.5*(outConservative[0] * *dT + midPointConservative[0] + inConservative[0]);
+  temp[1] = 0.5*(outConservative[1] * *dT + midPointConservative[1] + inConservative[1]);
+  temp[2] = 0.5*(outConservative[2] * *dT + midPointConservative[2] + inConservative[2]);
 
-  outConservative[0] = outConservative[0] <= EPS ? EPS : outConservative[0];
-  outConservative[3] = inConservative[3];
+  temp[0] = temp[0] <= EPS ? EPS : temp[0];
+  temp[3] = inConservative[3];
 
   //call to ToPhysicalVariables inlined
-  float TruncatedH = outConservative[0] < EPS ? EPS : outConservative[0];
-  out[0] = outConservative[0];
-  out[1] = outConservative[1] / TruncatedH;
-  out[2] = outConservative[2] / TruncatedH;
-  out[3] = outConservative[3];
+  float TruncatedH = temp[0] < EPS ? EPS : temp[0];
+  out[0] = temp[0];
+  out[1] = temp[1] / TruncatedH;
+  out[2] = temp[2] / TruncatedH;
+  out[3] = temp[3];
+  
+  midPointConservative[0] = 0.0f;
+  midPointConservative[1] = 0.0f;
+  midPointConservative[2] = 0.0f;
+  midPointConservative[3] = 0.0f;
+  outConservative[0] = 0.0f;
+  outConservative[1] = 0.0f;
+  outConservative[2] = 0.0f;
+  outConservative[3] = 0.0f;
 }
