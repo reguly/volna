733b7a14c5c1468037b0e4c1ced242115524dc3c
Istvan Reguly <eisox069@ui2.gpu.rl.ac.uk>
2013-11-26 10:52:34 +0000

    Documentation

--- sp/read_gauges.py
+++ sp/read_gauges.py
@@ -5,10 +5,27 @@ import datetime
 import h5py
 import numpy as np
 
-f = h5py.File("gauges.h5","r")
+#By default, open gauges.h5
+file = 'gauges.h5'
+#But if a filename is passed in the command line, open that
+if len(sys.argv)>1:
+  file = str(sys.argv[1])
+f = h5py.File(file,"r")
+
+#Read the dimensions of the data
+#dims[0] is the number of gauges+1 (for timestamps)
+#dims[1] is the number of timesteps
 dims = f['/dims']
+
+#Read in gauge data
 data = f['/gauges']
+
+#Print some data
+#to access gauge N (indexed 1...M) at time T:
+#data[T*dims[0]+N]
 print 'timestamp','gauge 10', 'gauge 23'
 for i in range(0,dims[1]):
-  print data[i*dims[0]], data[i*dims[0]+10], data[i*dims[0]+23]
+  print data[i*dims[0]], data[i*dims[0]+11], data[i*dims[0]+24]
+
+#Close data file
 f.close()
