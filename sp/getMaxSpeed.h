ba685164ebbbee31bd445cf95f56e2be392b78f2
Istvan Reguly <regulyistvan@gmail.com>
2017-08-11 17:20:34 +0200

    Typo bug in getMaxSpeed

--- sp/getMaxSpeed.h
+++ sp/getMaxSpeed.h
@@ -1,7 +1,7 @@
 inline void getMaxSpeed(const float* values, float* currentMaxSpeed) {
   /*float tmp = values[0]+values[3];
   *currentMaxSpeed = *currentMaxSpeed > tmp ? *currentMaxSpeed : tmp;*/
-  if (sqrt(values[1]*values[1]+values[2]*values[2]) > sqrt(currentMaxSpeed[1]*currentMaxSpeed[1]+currentMaxSpeed[2]*currentMaxSpeed[3])) {
+  if (sqrt(values[1]*values[1]+values[2]*values[2]) > sqrt(currentMaxSpeed[1]*currentMaxSpeed[1]+currentMaxSpeed[2]*currentMaxSpeed[2])) {
     currentMaxSpeed[0] = values[0];
     currentMaxSpeed[1] = values[1];
     currentMaxSpeed[2] = values[2];
