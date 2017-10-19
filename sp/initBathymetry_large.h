2d750e84b48a1f41790cf172b075998c35270e6f
Istvan Reguly <regulyistvan@gmail..com>
2017-10-19 11:07:16 +0200

    Experimental vectorisation

--- sp/initBathymetry_large.h
+++ sp/initBathymetry_large.h
@@ -1,48 +1,48 @@
 inline void initBathymetry_large(float *values, const float *cellCenter,
-	const float *node0, const float *node1, const float *node2,
-	const float *bathy0, const float *bathy1, const float *bathy2) {
+ const float *node0, const float *node1, const float *node2,
+ const float *bathy0, const float *bathy1, const float *bathy2) {
 
-	//First, check whether the cell is in the large triangle
-	bool isInside = false;
 
-  	// First, check if the point is in the bounding box of the triangle
-  	// vertices (else, the algorithm is not nearly robust enough)
-	float xmin = MIN(MIN(node0[0], node1[0]), node2[0]);
-	float xmax = MAX(MAX(node0[0], node1[0]), node2[0]);
-	float ymin = MIN(MIN(node0[1], node1[1]), node2[1]);
-	float ymax = MAX(MAX(node0[1], node1[1]), node2[1]);
+ bool isInside = false;
 
-	if ( ( cellCenter[0] < xmin ) || ( cellCenter[0] > xmax ) ||
-		( cellCenter[1] < ymin ) || ( cellCenter[1] > ymax ) ) {
-		isInside = false;
-	}else{
-	    // Case where the point is in the bounding box. Here, if abc is not
-	    // Check if the Triangle vertices are clockwise or
-	    // counter-clockwise
-		float insider = 1.0f;
-		float p[2] = {cellCenter[0], cellCenter[1]};
 
-#define ORIENT2D(pA, pB, pC) (pA[0] - pC[0]) * (pB[1] - pC[1]) - (pA[1] - pC[1]) * (pB[0] - pC[0])
-    if ( ORIENT2D(node0, node1, node2) > 0 ) {  // counter clockwise
-    	insider =  ORIENT2D( node0, p, node2);
-    	insider *= ORIENT2D( node0, node1, p);
-    	insider *= ORIENT2D( node1, node2, p);
+
+ float xmin = MIN(MIN(node0[0], node1[0]), node2[0]);
+ float xmax = MAX(MAX(node0[0], node1[0]), node2[0]);
+ float ymin = MIN(MIN(node0[1], node1[1]), node2[1]);
+ float ymax = MAX(MAX(node0[1], node1[1]), node2[1]);
+
+ if ( ( cellCenter[0] < xmin ) || ( cellCenter[0] > xmax ) ||
+  ( cellCenter[1] < ymin ) || ( cellCenter[1] > ymax ) ) {
+  isInside = false;
+ }else{
+
+
+
+  float insider = 1.0f;
+  float p[2] = {cellCenter[0], cellCenter[1]};
+
+
+    if ( (node0[0] - node2[0]) * (node1[1] - node2[1]) - (node0[1] - node2[1]) * (node1[0] - node2[0]) > 0 ) {
+     insider = (node0[0] - node2[0]) * (p[1] - node2[1]) - (node0[1] - node2[1]) * (p[0] - node2[0]);
+     insider *= (node0[0] - p[0]) * (node1[1] - p[1]) - (node0[1] - p[1]) * (node1[0] - p[0]);
+     insider *= (node1[0] - p[0]) * (node2[1] - p[1]) - (node1[1] - p[1]) * (node2[0] - p[0]);
     }
-    else {      // clockwise
-    	insider =  ORIENT2D( node0, p, node1);
-    	insider *= ORIENT2D( node0, node2, p);
-    	insider *= ORIENT2D( node2, node1, p);
+    else {
+     insider = (node0[0] - node1[0]) * (p[1] - node1[1]) - (node0[1] - node1[1]) * (p[0] - node1[0]);
+     insider *= (node0[0] - p[0]) * (node2[1] - p[1]) - (node0[1] - p[1]) * (node2[0] - p[0]);
+     insider *= (node2[0] - p[0]) * (node1[1] - p[1]) - (node2[1] - p[1]) * (node1[0] - p[0]);
     }
     isInside = insider >= 0.0f;
-	}
+ }
 
-  //If it's inside, then calculate the bathymetry value, with a simple linear interpolation
+
   if (isInside) {
-    //get the normal vector
-    float a =  (node1[1]-node0[1])*(*bathy2-*bathy0)-(node2[1]-node0[1])*(*bathy1-*bathy0);
+
+    float a = (node1[1]-node0[1])*(*bathy2-*bathy0)-(node2[1]-node0[1])*(*bathy1-*bathy0);
     float b = -(node1[0]-node0[0])*(*bathy2-*bathy0)+(node2[0]-node0[0])*(*bathy1-*bathy0);
-    float c =  (node1[0]-node0[0])*(node2[1]-node0[1])-(node2[0]-node0[0])*(node1[1]-node0[1]);
-    //get z so that the vector is orthogonal to the normal
+    float c = (node1[0]-node0[0])*(node2[1]-node0[1])-(node2[0]-node0[0])*(node1[1]-node0[1]);
+
     values[3] += *bathy0 - (a*(cellCenter[0]-node0[0]) + b*(cellCenter[1]-node0[1]))/c;
   }
 }
