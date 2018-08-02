88e804ed0c23e789c3cf1fd99c0f9d1258ceb749
Dan Giles <daniel.giles@ucdconnect.ie>
2018-08-02 10:16:53 +0100

    Second order Source Discretisation - Audusse Centered Source term added

--- sp/SpaceDiscretization.h
+++ sp/SpaceDiscretization.h
@@ -12,7 +12,9 @@ inline void SpaceDiscretization(float *left, //OP_INC
   left[0] -= (edgeFluxes[0])/cellVolumes0[0];
   left[1] -= (edgeFluxes[1] + bathySource[0] * edgeNormals[0])/cellVolumes0[0];
   left[2] -= (edgeFluxes[2] + bathySource[0] * edgeNormals[1])/cellVolumes0[0];
-
+  // Added centered Source Term
+  left[1] += (bathySource[2] *edgeNormals[0])/cellVolumes0[0];
+  left[2] += (bathySource[2] *edgeNormals[1])/cellVolumes0[0];
   }else{
   left[0] -= 0.0f;
   left[0] -= 0.0f;
@@ -24,7 +26,9 @@ inline void SpaceDiscretization(float *left, //OP_INC
     right[0] += edgeFluxes[0]/cellVolumes1[0];
     right[1] += (edgeFluxes[1] + bathySource[1] * edgeNormals[0])/cellVolumes1[0];
     right[2] += (edgeFluxes[2] + bathySource[1] * edgeNormals[1])/cellVolumes1[0];
-
+    // Added centered Source Term
+    right[1] -= (bathySource[3] *edgeNormals[0])/cellVolumes1[0];
+    right[2] -= (bathySource[3] *edgeNormals[1])/cellVolumes1[0];
     }else{
     right[0] += 0.0f;
     right[1] += 0.0f;
