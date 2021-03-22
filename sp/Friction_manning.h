d579153f3ab92158c681f39a19992b64055b9b86
Dan Giles <daniel.giles@ucdconnect.ie>
2021-03-22 13:30:51 +0000

    Update to Friction for dry cells

--- sp/Friction_manning.h
+++ sp/Friction_manning.h
@@ -7,8 +7,15 @@ inline void Friction_manning(const float *dT,const float *M_n, //OP_RW, discard
   F = g* (*M_n * *M_n) *speed;
   float TruncatedH = values[0] < 10.f*EPS ? EPS : values[0];
   F = F/(pow(TruncatedH,4.0f/3.0f));
+	if (values[0] <= EPS){
+     values[0] = EPS;
+     values[1] = 0.0f;
+     values[2] = 0.0f;
+     values[3] = values[3];
+  } else {
   values[0] = TruncatedH;
   values[1] = values[1] / (1.0f + F * *dT);
   values[2] = values[2] / (1.0f + F * *dT);
   values[3] = values[3];
+  }
 }
