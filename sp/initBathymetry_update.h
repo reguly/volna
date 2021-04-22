4267d8446044c594b37ba6dcade00a5a633a757d
Dan Giles <daniel.giles@ucdconnect.ie>
2021-04-22 10:00:16 +0100

    Dealing with changing bathymetry files - `old-format'

--- sp/initBathymetry_update.h
+++ sp/initBathymetry_update.h
@@ -1,17 +1,17 @@
-inline void initBathymetry_update(float *values, const float *zmin, const int *firstTime) {
+inline void initBathymetry_update(float *values, const float *z_zero, const float *zmin, const int *firstTime) {
     if (*firstTime){
-      if (values[3] >0.0f){
+      if (*z_zero > 0.0f){
         values[0] = EPS;
-        values[3] = -1.0f* *zmin + values[3];
+        values[3] = -1.0f* *zmin + *z_zero;
       } else {
-        values[0] = -1.0f*values[3];
+        values[0] = -1.0f* *z_zero;
         values[3] = -1.0f* *zmin;
       }
     } else {
-      if (values[3] > 0.0f){
-        values[3] = -1.0f* *zmin + values[3] + values[0];
+      if (*z_zero > 0.0f){
+        values[3] = *z_zero + values[0];
       } else {
-        values[3] = (values[0]+values[3]) -1.0f* *zmin;
+        values[3] = values[0] + *z_zero - *zmin;
       }
 
     }
